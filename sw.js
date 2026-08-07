bc007c5bf8bfe9f80f2f15f989a249d8b34dd6b4
LimaCrysis <rimacleese77@gmail.com>
2026-08-08 08:25:28 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'my-fishing-life-v2.3.0';
+const CACHE = 'my-fishing-life-v2.4.0';
 const ASSETS = ['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./kisu.jpg','./seabass.jpg','./aji.jpg','./saba.jpg','./kasago.jpg','./aigo.jpg','./haokoze.jpg','./gonzui.jpg'];
 
 self.addEventListener('install', event => {
